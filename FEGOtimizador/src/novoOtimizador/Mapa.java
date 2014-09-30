13752064eac7693b9d0aa14cb0726270af77a03d
Pedrofoz <pedroafoz@hotmail.com>
2014-09-30 19:20:25 -0300

    Encontrando o objeto Rua onde encontra-se o ponto de localização inicial.

--- FEGOtimizador/src/novoOtimizador/Mapa.java
+++ FEGOtimizador/src/novoOtimizador/Mapa.java
@@ -1,6 +1,7 @@
 package novoOtimizador;
 
 import java.util.ArrayList;
+import java.lang.*;
 import java.util.Iterator;
 
 import lejos.robotics.navigation.Waypoint;
@@ -45,11 +46,14 @@ public class Mapa {
 	//Método para encontrar o menor caminho
 	public ArrayList<Rua> menorCaminho(Destinos[] destino )
 	{
+		// destino[0] = ponto atual
+		// destino[1] = ponto final
 		//Variaveis auxiliares
 		ArrayList<Rua> aL = new ArrayList<Rua>();
 		Iterator it = ruas.iterator();
 		int a = 0;
 		Rua r = new Rua();
+		
 		
 		while(it.hasNext()) //Percorrer o arrayList até o fim
 		{
@@ -60,10 +64,40 @@ public class Mapa {
 			}
 		}
 		it = aL.iterator(); //aL com as ruas de mesmo nome
+		
+		
 		/*
 		 * Com o nome da Rua, encontrar qual Rua, das ruas (trechos) com o mesmo nome, o robô está!
 		 * Encontrar através da menor distância do ponto de destino[0] aos pontos de Inicio e Fim das ruas...
 		 */
+		double d;
+		Rua atual;
+		r = (Rua) it.next();
+		d = Math.sqrt((Math.pow((destino[0].getLocalizacao().getX()-r.getInicio().getX()),2.0))+(Math.pow((destino[0].getLocalizacao().getY()-r.getInicio().getY()),2.0)))+Math.sqrt((Math.pow((destino[0].getLocalizacao().getX()-r.getFim().getX()),2.0))+(Math.pow((destino[0].getLocalizacao().getY()-r.getFim().getY()),2.0)));
+		atual = r;
+		double x;
+		while(it.hasNext())
+		{
+			r = (Rua) it.next();
+			x = Math.sqrt((Math.pow((destino[0].getLocalizacao().getX()-r.getInicio().getX()),2.0))+(Math.pow((destino[0].getLocalizacao().getY()-r.getInicio().getY()),2.0)))+Math.sqrt((Math.pow((destino[0].getLocalizacao().getX()-r.getFim().getX()),2.0))+(Math.pow((destino[0].getLocalizacao().getY()-r.getFim().getY()),2.0)));
+			if (d > x)
+			{
+				d = x;
+				atual = r;
+			}
+		}
+		//Logo, o destino[0] (atual localiczação) esta na rua "atual"
+		
+		
+		/*
+		 * Agora, encontrar o menor caminho (sequencia de ruas válidas)
+		 * uma vez que já sabemos qual a rua inicial da sequência..
+		 **********************************************************************************************************
+		 * Propõe-se um algoritmo que leve em conta a menor distancia entre
+		 * os pontos de inicio e fim da rua em questão até o ponto de destino final
+		 * (deve-se considerar o sentido de fluxo pelas ruas, e cascatear as respostas, ou seja, montar aL final)
+		 */
+		
 		return aL;
 	}
 }
