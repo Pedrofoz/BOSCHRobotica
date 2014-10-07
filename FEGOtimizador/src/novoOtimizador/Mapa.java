29edf9f241a0fd22cc8e4ee6e1ce2d556ac67ee2
Pedrofoz <pedroafoz@hotmail.com>
2014-10-07 20:21:37 -0300

    Inicio do desenvolvimento da rotina que encontra os caminhos possíveis; Análise das conexão entre ruas; Encontrando rua do ponto de destino final;

--- FEGOtimizador/src/novoOtimizador/Mapa.java
+++ FEGOtimizador/src/novoOtimizador/Mapa.java
@@ -71,10 +71,10 @@ public class Mapa {
 		 * Encontrar através da menor distância do ponto de destino[0] aos pontos de Inicio e Fim das ruas...
 		 */
 		double d;
-		Rua atual;
+		Rua atualIn;
 		r = (Rua) it.next();
 		d = Math.sqrt((Math.pow((destino[0].getLocalizacao().getX()-r.getInicio().getX()),2.0))+(Math.pow((destino[0].getLocalizacao().getY()-r.getInicio().getY()),2.0)))+Math.sqrt((Math.pow((destino[0].getLocalizacao().getX()-r.getFim().getX()),2.0))+(Math.pow((destino[0].getLocalizacao().getY()-r.getFim().getY()),2.0)));
-		atual = r;
+		atualIn = r;
 		double x;
 		while(it.hasNext())
 		{
@@ -83,21 +83,116 @@ public class Mapa {
 			if (d > x)
 			{
 				d = x;
-				atual = r;
+				atualIn = r;
 			}
 		}
 		//Logo, o destino[0] (atual localiczação) esta na rua "atual"
-		
+	
+		//Ocorre o mesmo para o ponto final
+		it = ruas.iterator();
+		aL = new ArrayList<Rua>();
+		while(it.hasNext()) //Percorrer o arrayList até o fim
+		{
+			r = (Rua) it.next();
+			if (destino[1].getNomeDaRua().equals(r.getNome()))
+			{
+				aL.add(r);
+			}
+		}
+		it = aL.iterator(); //aL com as ruas de mesmo nome
+		Rua atualFim = new Rua();
+		r = (Rua) it.next();
+		d = Math.sqrt((Math.pow((destino[1].getLocalizacao().getX()-r.getInicio().getX()),2.0))+(Math.pow((destino[1].getLocalizacao().getY()-r.getInicio().getY()),2.0)))+Math.sqrt((Math.pow((destino[1].getLocalizacao().getX()-r.getFim().getX()),2.0))+(Math.pow((destino[1].getLocalizacao().getY()-r.getFim().getY()),2.0)));
+		atualFim = r;
+		while(it.hasNext())
+		{
+			r = (Rua) it.next();
+			x = Math.sqrt((Math.pow((destino[1].getLocalizacao().getX()-r.getInicio().getX()),2.0))+(Math.pow((destino[1].getLocalizacao().getY()-r.getInicio().getY()),2.0)))+Math.sqrt((Math.pow((destino[1].getLocalizacao().getX()-r.getFim().getX()),2.0))+(Math.pow((destino[1].getLocalizacao().getY()-r.getFim().getY()),2.0)));
+			if (d > x)
+			{
+				d = x;
+				atualFim = r;
+			}
+		}
+				
+		//Logo, o destino[0] (atual localiczação) esta na rua "atualIn"
+		//Logo, o destino[1] (atual localiczação) esta na rua "atualFim"
+	
 		
 		/*
 		 * Agora, encontrar o menor caminho (sequencia de ruas válidas)
 		 * uma vez que já sabemos qual a rua inicial da sequência..
 		 **********************************************************************************************************
+		 *
 		 * Propõe-se um algoritmo que leve em conta a menor distancia entre
 		 * os pontos de inicio e fim da rua em questão até o ponto de destino final
 		 * (deve-se considerar o sentido de fluxo pelas ruas, e cascatear as respostas, ou seja, montar aL final)
 		 */
-		
+			
+		Rua[][] caminhosPossiveis = {};
+		aL = new ArrayList<Rua>();
+		int l = 0;
+		int c = 0;
+		Rua atual = atualIn;
+		Rua[] ruasPossiveis = {};
+		boolean nCaminhosFinalizados = false;
+		while(nCaminhosFinalizados == false)
+		{
+			if(atual.getDirecao()[0] == true)
+			{				
+				//Inicio -> Fim
+				ruasPossiveis = verConexoes(atual.getFim());
+				for (int i = 0; i < ruasPossiveis.length; i++);
+				{
+					//Cascatear o processo de conexoes
+					//Adicionando toda nova conexao a uma nova linha e copiando as colunas anteriores
+					/*
+					 * [INICIO][a][b][c]
+					 * 	                [d][FINAL]
+					 * 	                [e][g][h][FINAL]
+					 * 
+					 * Ao final, preencher as linhas com colunas vazias com as colunas anteriores e considerar
+					 * apenas as que tiverem [FINAL] como último elemento, ou seja:
+					 * 
+					 * 1) [INICIO][a][b][c][d][FINAL]
+					 * 2) [INICIO][a][b][c][e][g][h][FINAL]
+					 */
+				}
+				
+			}
+			if(atual.getDirecao()[1] == true)
+			{
+				//Fim -> Inicio
+				//Executar similarmente ao anterior (Inicio -> Fim)
+			}
+			
+			/*
+			 * Adicionar método para comparar os caminhos por tempo e distancia
+			 * Encontrando o desejado
+			 */
+			//nCaminhosFinalizados = true;
+		}
 		return aL;
+	}
+	
+	//Analisa todas as ruas que fazem conexão com um determinado Waypoint
+	public Rua[] verConexoes(Waypoint a)
+	{
+		Rua[] r = {};
+		Rua ruaux = new Rua();
+		ArrayList<Rua> auxL = new ArrayList<Rua>();
+		Iterator it = ruas.iterator();
+		int i = 0;
+		while(it.hasNext())
+		{
+			ruaux = (Rua) it.next();
+			//Viabiliza a conexao por sentidos permitidos
+			if ((ruaux.getInicio().equals(a) && ruaux.getDirecao()[0] == true) || (ruaux.getFim().equals(a) && ruaux.getDirecao()[1] == true))
+			{
+				r[i] = ruaux;
+				i++;
+			}
+		}
+		return r;
 	}
 }
